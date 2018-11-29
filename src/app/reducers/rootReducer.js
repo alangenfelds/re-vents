a92cb033558424f3f880efb865fd67db00896bcb
Langenfelds, Arturs <a.langenfelds@a1qa.com>
2018-11-29 07:59:28 +0200

    added redux forms

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
+import { reducer as FormReducer } from 'redux-form'
+
 import testReducer from '../../features/Testarea/testReducer';
 import eventReducer from '../../features/event/eventReducer';
 
 const rootReducer = combineReducers({
     test: testReducer,
-    eventsReducer: eventReducer
+    eventsReducer: eventReducer,
+    form: FormReducer,
 })
 
 export default rootReducer
